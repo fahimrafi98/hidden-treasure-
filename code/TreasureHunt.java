8234b9c84ecacf4546856a66805ebe632dc7ec4c
Fahim Ahmed Rafi <farafi@mun.ca>
2022-07-24 19:12:41 -0230

    Update TreasureHunt.java

--- code/TreasureHunt.java
+++ code/TreasureHunt.java
@@ -3,10 +3,6 @@ import javax.swing.*;
 import java.awt.event.*;
 
 // Program to create a Treasure Hunt game with a GUI
-// Author: Katelyn Power
-// Student #: 201442944
-// COMP 2005 Assignment #1, due Sept. 24 at 11:59pm
-// Written with the help of Mark Hatcher's Simple GUI Demonstration 1&2
 
 public class TreasureHunt
 {
